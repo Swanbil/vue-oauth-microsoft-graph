671b9d9795635b91b162d84e6eeb5476f9e5e6b8
Swan <swan.bilek@efrei.net>
2021-09-21 14:36:10 +0200

    Ex 6: create BaseLayout that uses slot API

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-    <base-header />
-    <HomePage />
-    <base-footer id="footer"/>
+    <base-layout id="bl">
+      <template v-slot:home>
+        <HomePage />
+      </template>
+    </base-layout >
   </div>
 </template>
 
 <script>
 import HomePage from './pages/HomePage.vue';
-import BaseHeader from './components/BaseHeader.vue';
-import BaseFooter from './components/BaseFooter.vue';
+//import BaseHeader from './components/BaseHeader.vue';
+//import BaseFooter from './components/BaseFooter.vue';
+import BaseLayout from './components/BaseLayout.vue';
 
 export default {
   name: 'App',
   components: {
     HomePage,
-    'base-header': BaseHeader,
-    'base-footer' : BaseFooter
+    //'base-header': BaseHeader,
+    //'base-footer' : BaseFooter,
+    'base-layout' : BaseLayout
   }
 }
 </script>
@@ -30,10 +34,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-#footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  
-}
+
 </style>
