ab0277ebc46fcdc093c40778aeb25fee2f845368
Swan <swan.bilek@efrei.net>
2021-09-21 15:25:20 +0200

    Ex 7: create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="button">
+    <b-button v-if="!this.disable" variant="success">
+        <slot></slot>
+    </b-button>
+    
+    <b-button v-else class ="disabled"  variant="success">
+        <slot></slot>
+    </b-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: ['disable']
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.disabled{
+    background-color:rgb(112, 112, 112);
+}
+</style>
--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,20 +1,29 @@
 <template>
   <div class="header bg-success d-flex flex-row" >
     <h3 class="p-2">
-        <b-icon icon="house-fill" aria-hidden="true"></b-icon>
-        Home
+        <base-button :disable="false">
+          <b-icon icon="house-fill" aria-hidden="true"></b-icon>
+          Home
+        </base-button>
     </h3>
     <h3 class="ms-auto p-2">
+      <base-button :disable="false">
         <b-icon icon="person-circle" aria-hidden="true"></b-icon>
         Username
+      </base-button>
     </h3>
+    <base-button/>
     
   </div>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue'
 export default {
   name: 'BaseHeader',
+  components: {
+    'base-button' : BaseButton
+  }
 }
 </script>
 
